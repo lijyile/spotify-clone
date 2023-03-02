bcb2477377da2e0d440990b49da030bb38dd94c8
lijyile <yilesudi4343@gmail.com>
2023-03-02 13:30:06 -0500

    done

---
+++ src/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="login">
+      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fgetheavy.com%2F2019%2F12%2F06%2Fheres-how-many-streams-metal-hard-rock-hardcore-bands-got-on-spotify-in-2019%2F&psig=AOvVaw2q6GQpkN3wJCHncoTtR5EU&ust=1677840648571000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCJiX99CJvf0CFQAAAAAdAAAAABAE"></img>
+    </div>
+  );
+}
+
+export default Login;
